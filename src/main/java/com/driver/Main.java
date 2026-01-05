8b2f7f883096921146a97dcb203de2bda4ee0f25
lavanyaasharma <145443246+lavanyaasharma@users.noreply.github.com>
2026-01-05 09:08:39 +0530

    Implement method overloading using Product class

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,33 @@ package com.driver;
 
 public class Main {
 
+    // Task 1: Create Product class inside Main
+    static class Product {
+
+        // Task 3
+        public int product(int x, int y) {
+            return x * y;
+        }
+
+        // Task 4 (Overloaded)
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        // Task 5 (Overloaded)
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // Task 2: Create object p
+        Product p = new Product();
+
+        // Calling all overloaded methods
+        System.out.println(p.product(2, 3));          // int, int
+        System.out.println(p.product(2, 3, 4));       // int, int, int
+        System.out.println(p.product(2.5, 3.5));      // double, double
+    }
 }
